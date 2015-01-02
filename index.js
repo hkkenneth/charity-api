ad1beca6680f293aab01120afd447de55c58a9d8
hkkenneth <hkkenneth@gmail.com>
2015-01-02 12:44:23 +0800

    Implemented POST /settings

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require("express"),
+    mongodb = require('mongodb'),
+    storage = require('./subscription.js');
+
+var app = express()
+  , bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;  // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();  // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/health-check)
+router.get('/health-check', function(req, res) {
+  res.json({ message: 'charity-api running properly' });
+});
+
+// more routes for our API will happen here
+router.post('/settings', function(req, res) {
+ // console.log(req.body.regId);
+ // console.log(req.body.preRegId);
+ // console.log(req.body.payload);
+  console.log(req.body);
+  storage.save('GCM', req.body.regId, req.body.payload, function(result){
+    console.log(result);
+    res.json({ message: 'done' });
+  });
+});
+
+// REGISTER OUR ROUTES
+app.use('', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+
---
+++ subscription.js
@@ -0,0 +1,30 @@
+module.exports = function() {
+  var MongoClient = require('mongodb').MongoClient;
+  var config = require('./config.js');
+  var MONGO_URL = config.mongoUrl;
+
+  return {
+    save: function save(type, id, settings, callback) {
+      MongoClient.connect(MONGO_URL, function(err, db) {
+        if (err) {
+          console.log('mongo connection error');
+          throw err;
+        }
+        var collection = db.collection('subscription');
+        var payload = {
+          type: type,
+          clientId: id,
+          settings: settings
+        };
+        collection.insert(payload, function(err, docs) {
+          if (err) {
+            console.log('mongo insertion error');
+            throw err;
+          }
+          db.close();
+          callback(docs);
+        });
+      });
+    }
+  }
+}();
